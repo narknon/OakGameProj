1defc5aa41f5a5029b8f9cdabd93e9d12b8c789c
Narknon <Narknon0@gmail.com>
2022-05-23 12:10:21 -0400

    Update DownloadableInventorySetData.h

--- Source/GbxInventory/Public/DownloadableInventorySetData.h
+++ Source/GbxInventory/Public/DownloadableInventorySetData.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "CoreMinimal.h"
+#include "DownloadableContentData.h"
 //CROSS-MODULE INCLUDE V2: -ModuleName=OnlineSubsystemUtils -ObjectName=DownloadableContentData -FallbackName=DownloadableContentData
 #include "DownloadableInventorySetData.generated.h"
+
+
 
 UCLASS()
 class GBXINVENTORY_API UDownloadableInventorySetData : public UDownloadableContentData {
